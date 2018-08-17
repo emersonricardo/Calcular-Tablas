c87f0ede0bdb8276d3aea882f3b21e5be0ffdc9d
Centro de Desarrollo De Software <cds@univo.edu.sv>
2018-08-17 12:54:40 -0600

    cambio2

---
+++ CalcularTablas/CalcularTablas/Controllers/tablaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalcularTablas.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Index
+        public ActionResult Tabla()
+        {
+            ViewBag.datos = false;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Tabla(int txttabla)
+        {
+            ViewBag.datos = true;
+            ViewBag.tabla = txttabla;
+            
+            return View();
+        }
+    }
+}
